87483a5d39fb652742b2142cbe99c18fed49975b
UbaldoFMelchor <ubaldofmelchor97@hotmail.com>
2022-04-27 14:14:37 -0500

    agregado test 4

--- test/views/UserView.test.js
+++ test/views/UserView.test.js
@@ -29,4 +29,12 @@ describe("Test for UserView", () => {
         expect(result.error).toMatch(/necesitan tener un valor válido/)
     });
 
+    test("create a user by a given valid payload", () => {
+        const payload = {username: "username", id: 1, name: "name"}
+        const result = UserView.createUser(payload)
+        expect(result.name).toBe("name")
+        expect(result.username).toBe("username")
+        expect(result.id).toBe(1)
+    })
+
 })
