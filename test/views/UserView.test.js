63739d983027510db8df08bbd5698a63e4ef7501
UbaldoFMelchor <ubaldofmelchor97@hotmail.com>
2022-04-27 12:57:44 -0500

    agregado test 2

--- test/views/UserView.test.js
+++ test/views/UserView.test.js
@@ -15,6 +15,12 @@ describe("Test for UserView", () => {
         // https://jestjs.io/docs/next/using-matchers#strings
         expect(result.error).toMatch(/payload no existe/)
 
+    });
+
+    test("Return an error object when try to create a new user with a payload with invalid properties", () => {
+        const payload = {username: null, name: 12, id: "id"}
+        const result = UserView.createUser(payload)
+        expect(result.error).toMatch(/necesitan tener un valor válido/)
     })
 
 })
