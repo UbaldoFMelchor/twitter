a70470dcc476695061929e6cc7e9e208b37f51df
UbaldoFMelchor <ubaldofmelchor97@hotmail.com>
2022-04-26 23:20:25 -0500

    añadido updateUserUsername

--- app/services/userServices.js
+++ app/services/userServices.js
@@ -4,10 +4,16 @@ class UserServices {
     static create(id,username,name) {
         return new User(id,username,name, "Sin Bio")
     }
+
     static getInfo() {
         return [1,"ubxidonitroso","Ubaldo","Sin Bio"]
     
     }
+
+    static updateUserUsername(user,username) {
+        user.setUsername = username        
+    }
+
 }
 
 module.exports = UserServices
